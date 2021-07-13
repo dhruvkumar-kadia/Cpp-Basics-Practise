f9d33a03bd51ecb42799be1a464ca37d7d61c47c
dhruvkumar-kadia <kadia.dhruv.dk@gmail.com>
2021-07-13 16:55:30 +0530

    first commit

---
+++ simple_intrest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int principal = 1000;
+    float rate = 10;
+    int time = 2;
+    float SI = (principal*rate*time)/100;
+
+    cout << SI << endl;
+
+}
